440168509e6a0b8a01fbce5c2919f6b68c0bd515
Phanbook <hello@phanbook.com>
2015-08-03 14:31:57 +0700

    fix small opsfiles

--- opsfiles/scripts/ubuntu/base.sh
+++ opsfiles/scripts/ubuntu/base.sh
@@ -2,4 +2,4 @@
 sudo apt-get -y install update
 sudo apt-get -y install build-essential
 sudo apt-get -y install software-properties-common
-sudo apt-get -y install git
+sudo apt-get -y install git wget
