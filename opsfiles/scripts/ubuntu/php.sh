f64fc5e189b952adf8dd8c66fcdaf327fe743f98
Phanbook <hello@phanbook.com>
2015-08-03 12:59:14 +0700

    fix dockerfiles :ocean:

--- opsfiles/scripts/ubuntu/php.sh
+++ opsfiles/scripts/ubuntu/php.sh
@@ -1,6 +1,7 @@
 
 # Install PHP
 # -qq implies -y --force-yes
+sudo apt-get -y update
 sudo apt-get install -y php5-cli php5-fpm php5-mysql php5-curl
 # Set PHP FPM to listen on TCP instead of Socket
 #sudo sed -i "s/listen =.*/listen = /var/run/php5-fpm.sock/" /etc/php5/fpm/pool.d/www.conf
