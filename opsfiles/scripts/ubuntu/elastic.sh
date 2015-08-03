b08d1da5f9dfc4e49a18e17101e5135700f0f47b
Phanbook <hello@phanbook.com>
2015-08-03 23:46:41 +0700

    fix opsfiles

--- opsfiles/scripts/ubuntu/elastic.sh
+++ opsfiles/scripts/ubuntu/elastic.sh
@@ -3,10 +3,12 @@
 sudo apt-get -y install openjdk-7-jre-headless
 
 #Install Elasticsearch
-wget -qO - https://packages.elastic.co/GPG-KEY-elasticsearch | sudo apt-key add -
-echo "deb http://packages.elastic.co/elasticsearch/1.6/debian stable main" | sudo tee -a /etc/apt/sources.list.d/elasticsearch-1.6.list
-sudo apt-get -y update
-sudo apt-get -y install elasticsearch
+# wget -qO - https://packages.elastic.co/GPG-KEY-elasticsearch | sudo apt-key add -
+# echo "deb http://packages.elastic.co/elasticsearch/1.6/debian stable main" | sudo tee -a /etc/apt/sources.list.d/elasticsearch-1.6.list
+# sudo apt-get -y update
+# sudo apt-get -y install elasticsearch
+wget https://download.elastic.co/elasticsearch/elasticsearch/elasticsearch-1.7.1.deb
+sudo dpkg -i elasticsearch-1.7.1.deb
 sudo update-rc.d elasticsearch defaults 95 10
 sudo service elasticsearch restart
 
@@ -14,5 +16,5 @@ sudo service elasticsearch restart
 
 #Plugin
 PATH_PLUGIN="/usr/share/elasticsearch/bin/plugin"
-sudo $PATH_PLUGIN install royrusso/elasticsearch-HQ
+"$PATH_PLUGIN" install royrusso/elasticsearch-HQ
 
