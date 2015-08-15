1379893db50fca8997c354fb17e8bf55d7af0e96
Thien <fcduythien@gmail.com>
2015-08-15 18:06:03 +0700

    fix dumpme data

--- tests/acceptance/Controller/AuthControllerCept.php
+++ tests/acceptance/Controller/AuthControllerCept.php
@@ -4,8 +4,8 @@ $I->wantTo('add a new user and see it in database');
 $I->amOnPage('/login');
 $I->click('Sign Up With Email');
 $I->fillField('firstname', 'Phanbook');
-$I->fillField('email', 'phanbook@phanbook.com');
-$I->fillField('username', 'phanbook');
+$I->fillField('email', 'hello@phanbook.com');
+$I->fillField('username', 'admin');
 $I->click('Sign in');
-$I->seeInDatabase('users', ['email' => 'phanbook@phanbook.com', 'username' => 'phanbook']);
+$I->seeInDatabase('users', ['email' => 'hello@phanbook.com', 'username' => 'admin']);
 
