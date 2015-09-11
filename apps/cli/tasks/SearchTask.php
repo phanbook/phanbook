3bb2a4b9edae238d2779789472f4e28ad161058e
Phanbook <hello@phanbook.com>
2015-09-11 12:36:02 +0700

    add dockblock for task

--- apps/cli/tasks/SearchTask.php
+++ apps/cli/tasks/SearchTask.php
@@ -1,18 +1,30 @@
 <?php
+/**
+ * Phanbook : Delightfully simple forum software
+ *
+ * Licensed under The GNU License
+ * For full copyright and license information, please see the LICENSE.txt
+ * Redistributions of files must retain the above copyright notice.
+ *
+ * @link    http://phanbook.com Phanbook Project
+ * @since   1.0.0
+ * @license http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt
+ */
 namespace Phanbook\Cli\Tasks;
 
 use Phalcon\CLI\Task;
 use Phanbook\Search\Indexer;
+use Phanbook\Tools\Cli\Output;
 
 class SearchTask extends Task
 {
     public function mainAction()
     {
-        echo "\n Init search Indexer  \n";
+        Output::stdout('Init search Indexer');
 
         $search = new Indexer();
         $search->indexAll();
 
-        echo "\n Phanbook search successfully \n";
+        Output::stdout('Phanbook search successfully');
     }
 }
