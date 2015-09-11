1e84a2de04c8aea04c09090900212aac9bcbb37a
Phanbook <hello@phanbook.com>
2015-09-11 20:02:54 +0700

    fix small

--- common/config/options.php
+++ common/config/options.php
@@ -1,7 +1,1 @@
-<?php return new \Phalcon\Config([
-                'application' => [
-                    'name'      => 'Phanbook',
-                    'tagline'   => 'A Q&A site Demo',
-                    'publicUrl' => 'http://phanbook.com'
-                ],
-            ]);
+<?php return new \Phalcon\Config([]);
