ec16c5f165bf79983729adf60775613e48f67a41
Serghei Iakovlev <serghei@phalconphp.com>
2016-10-25 17:19:54 +0300

    Fixed test suites

--- tests/_support/Helper/Acceptance.php
+++ tests/_support/Helper/Acceptance.php
@@ -8,6 +8,6 @@ use Codeception\Module;
  * Here you can define custom actions
  * all public methods declared in helper class will be available in $I
  */
-class Functional extends Module
+class Acceptance extends Module
 {
 }
