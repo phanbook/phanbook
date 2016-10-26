8d34a7ab3fb162e805b1b79b7c6f2f1a417c7dae
Serghei Iakovlev <serghei@phalconphp.com>
2016-10-26 18:37:39 +0300

    Cleaned testing environment and config

--- tests/_bootstrap.php
+++ tests/_bootstrap.php
@@ -1,3 +1,18 @@
 <?php
+/**
+ * Phanbook : Delightfully simple forum software
+ *
+ * Licensed under The GNU License
+ * For full copyright and license information, please see the LICENSE.txt
+ * Redistributions of files must retain the above copyright notice.
+ *
+ * @link    http://phanbook.com Phanbook Project
+ * @since   1.0.0
+ * @license http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt
+ */
 
 putenv('APPLICATION_ENV=testing');
+
+ini_set('memory_limit', '-1');
+
+require dirname(dirname(__FILE__)) . '/bootstrap/autoloader.php';
--- tests/_config/bootstrap.php
+++ tests/_config/bootstrap.php
@@ -6,20 +6,12 @@
  * For full copyright and license information, please see the LICENSE.txt
  * Redistributions of files must retain the above copyright notice.
  *
- * @link          http://phanbook.com Phanbook Project
- * @since         1.0.0
- * @license       http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt
+ * @link    http://phanbook.com Phanbook Project
+ * @since   1.0.0
+ * @license http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt
  */
 use Phalcon\Mvc\Application;
 
-error_reporting(E_ALL);
-
-// Set internal character encoding to UTF-8.
-//mb_internal_encoding('UTF-8');
-ini_set('memory_limit', '-1');
-
-require ROOT_DIR . '/vendor/autoload.php';
-require ROOT_DIR . '/core/config/loader.php';
 require ROOT_DIR . '/core/config/services.php';
 
 /**
@@ -37,4 +29,4 @@ $application->setDI($di);
  */
 $application->registerModules(require ROOT_DIR . '/core/config/modules.php');
 
-return new \Phalcon\Mvc\Application($di);
+return new Application($di);
