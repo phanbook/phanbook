16aa8cd69f0e9bf33fdbc808eda1037f97698bf4
Serghei Iakovlev <serghei@phalconphp.com>
2016-10-27 21:15:17 +0300

    Removed outdated libs

--- opsfiles/scripts/app.sh
+++ opsfiles/scripts/app.sh
@@ -1,13 +1,19 @@
 #!/usr/bin/env bash
+
+COMPOSER_PATH=$(which composer 2> /dev/null)
+if [ -z "$COMPOSER_PATH" ]; then
+	curl -sS https://getcomposer.org/installer | php -- --install-dir=/usr/local/bin --filename=composer
+fi
+
 #cp app/config/config.example.php app/config/config.php
 echo ">>Install composer"
 cd "${ROOT_DIR}"
 
 if [[ ! -d "vendor" && "$ENV" == "production" ]]; then
-    php composer.phar install --no-ansi --no-dev --no-interaction --no-progress --no-scripts --optimize-autoloader
+    composer install --no-ansi --no-dev --no-interaction --no-progress --no-scripts --optimize-autoloader
 fi
 if [[ ! -d "vendor" && "$ENV" == "development" ]]; then
-    php composer.phar install --no-ansi --no-interaction --no-progress --no-scripts --optimize-autoloader
+    composer install --no-ansi --no-interaction --no-progress --no-scripts --optimize-autoloader
 fi
 if [[ ! -f "core/config/config.php" && "$ENV" == "development" ]]; then
     cp core/config/config.vagrant.php core/config/config.php
