d2d6677923c2115586ea92ab75354a1633c041fb
Serghei Iakovlev <serghei@phalconphp.com>
2016-10-29 00:15:27 +0300

    Initial Russian translation (frontpage)

---
+++ core/lang/messages/ru.php
@@ -0,0 +1,90 @@
+<?php
+
+$messages = [
+    // A
+    'A non-profit, open source Q&A site, discussion and blogging platform.' => 'Некоммерческий, открытый проект вопрсосв и ответов, обсуждений и блогинг платформа. ',
+    'About Us' => 'О нас',
+    'Answers' => 'Ответы',
+    'Ask any question and you be sure find your answer...' => 'Задайте любой вопрос и вы обязательно найдете ответ на него...',
+    'Ask Now' => 'Задать вопрос',
+    'Ask Question' => 'Задать вопрос',
+    // B
+    'Badges' => 'Знаки',
+    'By Phanbook' => 'Разработано Phanbook',
+    // C
+    'Change Password' => 'Изменить пароль',
+    'Contact' => 'Контакты',
+    'Contact Us' => 'Связяться с нами',
+    // D
+    'Dashboard' => 'Кабинет',
+    // E
+    'Edit Profile' => 'Изменить профиль',
+    'Edit Setting' => 'Изменить настройки',
+    'Edit Theme' => 'Изменить тему',
+    // F
+    'flag_question' => 'Тревога',
+    'Follow along' => 'Следите за нами',
+    'Followers' => 'Подписчиков',
+    // G
+    'GitHub Project' => 'Мы на GitHub',
+    // H
+    'Highest points' => 'Самые популярные',
+    'Home' => 'Главная',
+    'Hot' => 'Горячее',
+    // I
+    'in progress' => 'не решён',
+    'Index' => 'Главная',
+    'Interesting' => 'Интересное',
+    "It’s one of the most popular website among developers." => 'Это один из самых популярных сайтов среди разработчиков.',
+    // J
+    'Join Now' => 'Присоединиться',
+    // K
+    // L
+    'Login Area' => 'Вход',
+    'logo' => 'логотип',
+    'Logout' => 'Выход',
+    // M
+    'Month' => 'За месяц',
+    'My Answers' => 'Мои ответы',
+    'My Questions' => 'Мои вопросы',
+    // N
+    'No Answers' => 'Без ответа',
+    // O
+    // P
+    'People like it' => 'Людей любят нас',
+    'Points' => 'Репутация',
+    // Q
+    'Questions' => 'Вопросы',
+    'Quick Links' => 'Быстрые ссылки',
+    // R
+    'Recent Questions' => 'Недавние вопросы',
+    'Router' => 'Роутер',
+    // S
+    'Search here...' => 'Поиск...',
+    'Settings' => 'Настройки',
+    'site design' => 'дизайн сайта',
+    'solved' => 'решён',
+    'Stats' => 'Статистика',
+    'Subscribe' => 'Подписаться',
+    'Support' => 'Поддержка',
+    'Support:' => 'Поддержка:',
+    // T
+    'Tags' => 'Метки',
+    'To RSS Feed' => 'На RSS рассылку',
+    // U
+    'Users' => 'Участники',
+    // V
+    'Views' => 'Просмотры',
+    // W
+    'We are Phanbook.' => 'Мы &mdash; Phanbook.',
+    'Week' => 'За неделю',
+    'Welcome!' => 'Добро пожаловать!',
+    'Who We Are' => 'Кто мы',
+    'With the Phanbook you can building easy it!' => 'Используя Phanbook, вы можете легко создать свой StackOverflow!',
+    // X
+    // Y
+    "You're probably heard of StackOverflow before, the biggest Question and Answer website." => 'Вы, наверное, слышали о сайте StackOverflow, главном сайте вопросов и ответов.',
+    // Z
+];
+
+return $messages;
