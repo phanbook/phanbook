14402187f71ff905cf021603d6a254d8fb19ca65
Serghei Iakovlev <serghei@phalconphp.com>
2016-10-29 22:56:04 +0300

    Cleaned not worked code from test task

--- core/modules/cli/tasks/TestsTask.php
+++ core/modules/cli/tasks/TestsTask.php
@@ -33,28 +33,10 @@ class TestsTask extends Task
         Output::stdout("Main Action");
     }
 
-    public function cmdAction()
-    {
-        $cmd = \Cli\Execute::singleton();
-        $success = $cmd->execute("whoami", __FILE__, __LINE__, $output);
-
-        Output::stdout("You're running this script under $output user");
-    }
-
     public function test2Action($paramArray)
     {
-        Output::stdout("First param: $paramArray[0]");
-        Output::stdout("Second param: $paramArray[1]");
-    }
-
-    /**
-     * Action to trigger a fatal error
-     */
-    public function fatalAction()
-    {
-        // trigger a fatal error w/ Class that doesn't exist
-        //new TriggerARuntimeFatal();
-        strpos();
+        Output::stdout('First param: ' . $paramArray[0]);
+        Output::stdout('Second param: ' . $paramArray[1]);
     }
 
     public function renderAction()
