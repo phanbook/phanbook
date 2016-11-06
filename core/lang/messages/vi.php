207f204d632e629dd0a42aba77c14f9c07c50430
Serghei Iakovlev <serghei@phalconphp.com>
2016-11-06 20:49:35 +0200

    [i18n] Amended Vietnamese translation [ci skip]

--- core/lang/messages/vi.php
+++ core/lang/messages/vi.php
@@ -1,14 +1,120 @@
 <?php
 
 $messages = [
-    'Jobs' => 'Tuyển dụng',
+    // A
+    'about' => 'Về chúng tôi',
+    'About Us' => 'Về chúng tôi',
+    'about_us' => 'Về chúng tôi',
+    'account_switched_successfully' => 'Bạn đã đổi tài khoản thành công',
+    'activation_code' => 'Mã kích hoạt',
+    'add' => 'Thêm',
+    'add_account' => 'Thêm tài khoản',
+    'add_campaign' => 'Thêm chiến dịch',
+    'add_contact_group' => 'Tạo nhóm liên lạc',
+    'answers' => 'Câu trả lời',
+    'ask_questions' => 'Đặt câu hỏi',
+    // B
+    'Badges' => 'Huy Chương',
+    'broadcast_sms' => 'Gửi tin nhắn đến tất cả',
+    // C
+    'campaigns' => 'Chiến dịch',
+    'capabilities' => 'Khả năng',
+    'comment' => 'Bình luận',
+    'company_name' => 'Tên Công Ty',
+    'company_profile' => 'Hồ sơ công ty',
+    'confirm' => 'Đồng ý',
+    'confirm_password' => 'Xác nhận mật khảu',
+    'contact_added_success' => 'Sổ liên lạc đã được thêm vào danh bạ',
+    'contact_us' => 'Liên hệ',
+    'contact_us_title' => 'Liên hệ với chúng tôi',
+    'contacts' => 'Liên lạc',
     'Contact' => 'Liên Hệ',
-    'Services' => 'Dịch vụ',
-    'About us' => 'Về chúng tôi',
+    'Contact Us' => 'Liên hệ với chúng tôi',
+    'country_code' => 'Mã Quốc gia',
+    'create_group' => 'Tạo nhóm',
+    // D
+    'date' => 'Ngày',
+    // E
+    'edit_profile' => 'Thay đổi Hồ Sơ',
+    'edit_user' => 'Thay đổi thông tin',
+    'edit_user_capabilities' => 'Edit User Capabilities',
+    'email' => 'E-mail',
+    'email_address' => 'Địa chỉ Email',
+    'email_already_registered' => 'Tài khoản Email đã được đăng kí',
+    'email_not_valid' => 'Địa chỉ Email không hợp lệ',
+    'enter_phone_number' => 'Nhập số điện thoại',
+    'enter_your_message' => 'Nhập nội dung tin nhắn',
+    'example_john_doe' => 'Example: John Doe',
+    // F
+    'field_is_required' => 'Trường %field% bắt buộc',
+    'firstname' => 'Họ',
+    'fullname' => 'Tên đầy đủ',
+    // G
+    'groups' => 'Nhóm',
+    // H
     'Home' => 'Trang chủ',
+    // I
+    'individual' => 'Cá nhân',
+    'internal_error_occurred' => 'Có lỗi xảy ra, chúng tôi sẽ quay lại trong vài phút nữa',
+    'invalid_activation_request' => 'Yêu cầu kích hoạt không hợp lệ',
+    // J
+    'Jobs' => 'Tuyển dụng',
+    // K
+    // L
+    'lastname' => 'Tên',
+    'login' => 'Đăng nhập',
+    'logout' => 'Đăng xuất',
+    // M
+    'manage_contacts' => 'Quản lý danh sách liên lạc',
+    'message' => 'Tin nhắn',
+    'myprofile' => 'Thông tin hồ sơ',
+    // N
+    'name' => 'Tên',
+    // O
+    // P
+    'password' => 'Mật Khẩu',
+    'password_not_match' => 'Mật khẩu không trùng khớp',
+    'password_too_short_min_chars' => 'Mật khẩu của bạn quá ngắn, vui lòng đổi chọn mật khẩu với %num% kí tự',
+    'phone_number' => 'Số điện thoại',
+    'prices' => 'Giá cả',
+    'products' => 'Sản phẩm',
+    // Q
     'Questions' => 'Câu hỏi',
-    'Contact us' => 'Liên hệ với chúng tôi'
-
+    // R
+    'receive' => 'Nhận',
+    'reports' => 'Báo cáo',
+    'required_field' => 'Trường bắt buộc',
+    // S
+    'search' => 'Tìm kiếm',
+    'select_contacts' => 'Chọn số liên lạc',
+    'select_group' => 'Chọn nhóm',
+    'send_activation_code' => 'Gửi lại mã kích hoạt',
+    'send_confirmation_mail' => 'Gửi email xác thực',
+    'send_message' => 'Gửi tin nhắn',
+    'send_sms' => 'Gửi tin nhắn',
+    'Services' => 'Dịch vụ',
+    'services' => 'Dịch vụ',
+    'settings' => 'Cài đặt',
+    'supports' => 'Hỗ Trợ',
+    'switch_account' => 'Đổi tài khoản',
+    // T
+    'telephone' => 'Số điện thoại',
+    'thanks_we_will_contact' => 'Cảm ơn, chúng tôi lẽ liên lạc lại với bạn trong vài giờ tới',
+    'this_field_is_required' => 'Trường bắt buộc',
+    'time' => 'Giờ',
+    // U
+    'use_form_to_invite_user' => 'Vui lòng sử dụng mẫu bên dưới để mời bạn của bạn',
+    'user_added_successfully' => 'Người dùng được thêm thành công',
+    'user_already_exists' => 'Người dùng đã tồn tại',
+    'Users' => 'Người dùng',
+    'user_saved_successfully' => 'Người dùng đã được lưu lại',
+    // V
+    'view_reports' => 'Xem báo cáo',
+    // W
+    // X
+    // Y
+    'you_have_no_users' => 'Bạn chưa có tài khoản, vui lòng chọn vào đường link bên dưới để tạo tài khoản mới',
+    // Z
 ];
 
 return $messages;
