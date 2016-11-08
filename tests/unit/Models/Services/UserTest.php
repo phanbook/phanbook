cc8c6893649e956f0a5e28f5cb5f2d579428cc94
Serghei Iakovlev <serghei@phalconphp.com>
2016-11-08 21:57:49 +0200

    Amended User Service tests

--- tests/unit/Models/Services/UserTest.php
+++ tests/unit/Models/Services/UserTest.php
@@ -42,6 +42,25 @@ class UserTest extends UnitTest
     }
 
     /** @test */
+    public function shouldFindUserByUsername()
+    {
+        $userService = new User();
+
+        $data = [
+            'username'  => $this->faker->userName,
+            'firstname' => $this->faker->name,
+            'lastname'  => $this->faker->name,
+            'email'     => $this->faker->email,
+            'bio'       => $this->faker->paragraph,
+            'birthdate' => $this->faker->date(),
+            'passwd'    => $this->faker->password,
+        ];
+
+        $this->tester->haveInDatabase('users', $data);
+        $this->assertInstanceOf(Entity::class, $userService->findFirstByUsername($data['username']));
+    }
+
+    /** @test */
     public function shouldFindUserById()
     {
         $userService = new User();
@@ -76,7 +95,26 @@ class UserTest extends UnitTest
         ];
 
         $this->tester->haveInDatabase('users', $data);
-        $this->assertInstanceOf(Entity::class, $userService->findFirstByEmail($data['email']));
+        $this->assertInstanceOf(Entity::class, $userService->getFirstByEmail($data['email']));
+    }
+
+    /** @test */
+    public function shouldGetUserByUsername()
+    {
+        $userService = new User();
+
+        $data = [
+            'username'  => $this->faker->userName,
+            'firstname' => $this->faker->name,
+            'lastname'  => $this->faker->name,
+            'email'     => $this->faker->email,
+            'bio'       => $this->faker->paragraph,
+            'birthdate' => $this->faker->date(),
+            'passwd'    => $this->faker->password,
+        ];
+
+        $this->tester->haveInDatabase('users', $data);
+        $this->assertInstanceOf(Entity::class, $userService->getFirstByUsername($data['username']));
     }
 
     /** @test */
@@ -95,7 +133,7 @@ class UserTest extends UnitTest
         ];
 
         $id = $this->tester->haveInDatabase('users', $data);
-        $this->assertInstanceOf(Entity::class, $userService->findFirstById($id));
+        $this->assertInstanceOf(Entity::class, $userService->getFirstById($id));
     }
 
     /**
@@ -132,6 +170,98 @@ class UserTest extends UnitTest
                 $userService->getFirstByEmail($id);
             }
         );
+    }
+
+    /** @test */
+    public function shouldThrowExpectedExceptionInCaseOfGetNonExistentEntityByUsername()
+    {
+        $userService = new User();
+
+        $this->tester->dontSeeInDatabase('users', ['username' => 'username']);
+        $this->tester->expectException(
+            new EntityNotFoundException('username', 'username'),
+            function () use ($userService) {
+                $userService->getFirstByUsername('username');
+            }
+        );
+    }
+
+    /** @test */
+    public function shouldDetectModerators()
+    {
+        $userService = new User();
+
+        $data = [
+            'username'  => $this->faker->userName,
+            'firstname' => $this->faker->name,
+            'lastname'  => $this->faker->name,
+            'email'     => $this->faker->email,
+            'bio'       => $this->faker->paragraph,
+            'birthdate' => $this->faker->date(),
+            'passwd'    => $this->faker->password,
+            'moderator' => 'Y',
+        ];
+
+        $id = $this->tester->haveInDatabase('users', $data);
+        $user = $userService->getFirstById($id);
+
+        $this->assertTrue($userService->isModerator($user));
+
+        $data = [
+            'username'  => $this->faker->userName,
+            'firstname' => $this->faker->name,
+            'lastname'  => $this->faker->name,
+            'email'     => $this->faker->email,
+            'bio'       => $this->faker->paragraph,
+            'birthdate' => $this->faker->date(),
+            'passwd'    => $this->faker->password,
+            'moderator' => 'N',
+        ];
+
+        $id = $this->tester->haveInDatabase('users', $data);
+        $user = $userService->getFirstById($id);
+
+        $this->assertFalse($userService->isModerator($user));
+    }
+
+    /**
+     * @test
+     * @dataProvider providerStatus
+     * @param int $status
+     * @param bool $expected
+     */
+    public function shouldDetectActivatedUsers($status, $expected)
+    {
+        $userService = new User();
+
+        $data = [
+            'username'  => $this->faker->userName,
+            'firstname' => $this->faker->name,
+            'lastname'  => $this->faker->name,
+            'email'     => $this->faker->email,
+            'bio'       => $this->faker->paragraph,
+            'birthdate' => $this->faker->date(),
+            'passwd'    => $this->faker->password,
+            'status'    => $status,
+        ];
+
+        $id = $this->tester->haveInDatabase('users', $data);
+        $user = $userService->getFirstById($id);
+
+        $this->assertTrue($expected === $userService->isActiveMember($user));
+    }
+
+    public function providerStatus()
+    {
+        return [
+            [Entity::STATUS_ACTIVE, true],
+            [Entity::STATUS_DISABLED, false],
+            [Entity::STATUS_PENDING, false],
+            [Entity::STATUS_INACTIVE, false],
+            [123, false],
+            [-90, false],
+            [0, false],
+        ];
     }
 
     public function providerInvalidId()
