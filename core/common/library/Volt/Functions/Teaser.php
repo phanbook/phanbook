01d29b918dd3f1d0342d92de57baa86647cd46f1
Serghei Iakovlev <serghei@phalconphp.com>
2016-11-15 00:49:08 +0200

    Introduced Phanbook\Common\Library\Volt\Functions\Teaser

---
+++ core/common/library/Volt/Functions/Teaser.php
@@ -0,0 +1,53 @@
+<?php
+/**
+ * Phanbook : Delightfully simple forum software
+ *
+ * Licensed under The GNU License
+ * For full copyright and license information, please see the LICENSE.txt
+ * Redistributions of files must retain the above copyright notice.
+ *
+ * @link    http://phanbook.com Phanbook Project
+ * @since   1.0.0
+ * @license http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt
+ */
+namespace Phanbook\Common\Library\Volt\Functions;
+
+use function Stringy\create as s;
+
+/**
+ * \Phanbook\Common\Library\Volt\Functions\Teaser
+ *
+ * @package Phanbook\Common\Library\Volt\Functions
+ */
+class Teaser
+{
+    /**
+     * Truncates the text to a given length.
+     *
+     * @param string $text
+     * @param int    $maxLen
+     * @param bool   $saveWords
+     * @param string $endWith
+     *
+     * @return string
+     */
+    public static function create($text, $maxLen = 35, $saveWords = true, $endWith = ' &hellip;')
+    {
+        $string = s($text);
+        $length = $string->length();
+
+        if ($length <= $maxLen) {
+            return $text;
+        }
+
+        $string->trimRight('. ');
+
+        if ($saveWords) {
+            while ($maxLen < $length && preg_match('/^\pL$/', $string->substr($maxLen, 1))) {
+                $maxLen++;
+            }
+        }
+
+        return $string->substr(0, $maxLen) . $endWith;
+    }
+}
