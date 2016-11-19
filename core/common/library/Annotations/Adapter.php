28df183a56ed54d95b8f47104982e157c00d1618
Serghei Iakovlev <serghei@phalconphp.com>
2016-11-19 17:59:20 +0200

    Added Annotations Adapter

---
+++ core/common/library/Annotations/Adapter.php
@@ -0,0 +1,72 @@
+<?php
+/**
+ * Phanbook : Delightfully simple forum software
+ *
+ * Licensed under The GNU License
+ * For full copyright and license information, please see the LICENSE.txt
+ * Redistributions of files must retain the above copyright notice.
+ *
+ * @link    http://phanbook.com Phanbook Project
+ * @since   1.0.0
+ * @license http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt
+ */
+namespace Phanbook\Common\Library\Annotations;
+
+use Phalcon\Annotations\Adapter as PhAdapter;
+
+/**
+ * \Phanbook\Common\Library\Annotations\AdapterTrait
+ *
+ * @package Phanbook\Common\Library\Annotations
+ */
+trait AdapterTrait
+{
+    protected function getClassNameFromFile($filePath)
+    {
+        $fp = fopen($filePath, 'r');
+
+        $class = '';
+        $namespace = '';
+        $buffer = '';
+        $i = 0;
+
+        while (!$class) {
+            if (feof($fp)) {
+                break;
+            }
+
+            $buffer .= fread($fp, 512);
+            $tokens = token_get_all($buffer);
+
+            if (strpos($buffer, '{') === false) {
+                continue;
+            }
+
+            for (; $i < count($tokens); $i++) {
+                if ($tokens[$i][0] === T_NAMESPACE) {
+                    for ($j = $i + 1; $j < count($tokens); $j++) {
+                        if ($tokens[$j][0] === T_STRING) {
+                            $namespace .= '\\' . $tokens[$j][1];
+                        } elseif ($tokens[$j] === '{' || $tokens[$j] === ';') {
+                            break;
+                        }
+                    }
+                }
+
+                if ($tokens[$i][0] === T_CLASS) {
+                    for ($j = $i + 1; $j < count($tokens); $j++) {
+                        if ($tokens[$j] === '{') {
+                            $class = $tokens[$i + 2][1];
+                        }
+                    }
+                }
+            }
+        }
+
+        if (empty($class)) {
+            return '';
+        }
+
+        return '\\' . ($namespace ? trim($namespace, '\\') . '\\' : '') . trim($class, '\\');
+    }
+}
