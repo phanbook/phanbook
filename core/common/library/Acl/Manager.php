c7163b6b9cd4916dbc402df2adbd458a69b4278f
Serghei Iakovlev <serghei@phalconphp.com>
2016-11-19 21:04:14 +0200

    Make Phanbook\Common\Library\Acl\Manager::introspect protected

--- core/common/library/Acl/Manager.php
+++ core/common/library/Acl/Manager.php
@@ -60,65 +60,6 @@ class Manager extends Injectable
     }
 
     /**
-     * Introspect the access objects (, initialize the ACL adapter) and get ACL adapter.
-     *
-     * @return AdapterInterface
-     */
-    public function introspect()
-    {
-        if (!$this->acl) {
-            /** @var \Phalcon\Cache\BackendInterface $cache */
-            $cache = $this->getDI()->getDataCache();
-            $acl   = $cache->get($this->config->get('cacheKey'));
-
-            if (!$acl || ENV_LOCAL === APPLICATION_ENV) {
-                $save = !$acl && ENV_PRODUCTION === APPLICATION_ENV;
-
-                /** @var \Phalcon\Acl\AdapterInterface $acl */
-                $adapter = $this->config->get('adapter');
-                $acl = new $adapter();
-                $acl->setDefaultAction(Acl::DENY);
-
-                /** @var Service\Role $rolesService */
-                $rolesService = $this->getDI()->getShared(Service\Role::class);
-                $roleNames = [];
-
-                foreach ($rolesService->getList() as $role) {
-                    $acl->addRole(new Acl\Role($role->name, $role->description));
-                    $roleNames[$role->id] = $role->name;
-                }
-
-                $acl->addResource(new Acl\Resource(self::ADMIN_AREA), 'access');
-                $acl->allow($rolesService->getOrCreateAdminRole()->getName(), self::ADMIN_AREA, 'access');
-
-                $objects = $this->addResources($acl);
-
-                foreach (Access::find() as $access) {
-                    /** @var Access $access */
-                    if (!isset($objects[$access->getObject()])) {
-                        continue;
-                    }
-
-                    $object = $objects[$access->getObject()];
-                    $value  = $access->getValue();
-
-                    if (in_array($access->getAction(), $object->actions) && ($value == 'allow' || $value == 'deny')) {
-                        $acl->$value($roleNames[$access->getRoleId()], $access->getObject(), $access->getAction());
-                    }
-                }
-
-                if ($save) {
-                    $cache->save($this->config->get('cacheKey'), $acl, $this->config->get('lifetime'));
-                }
-            }
-
-            $this->acl = $acl;
-        }
-
-        return $this->acl;
-    }
-
-    /**
      * Gets access object.
      *
      * @param string $filePath
@@ -173,6 +114,91 @@ class Manager extends Injectable
         $object->path = $filePath;
 
         return $object;
+    }
+
+    /**
+     * Check whether a role is allowed to access an action from a resource
+     *
+     * @param mixed $roleName
+     * @param mixed $resourceName
+     * @param mixed $access
+     * @param array $parameters
+     * @return bool
+     */
+    public function isAllowed($roleName, $resourceName, $access, array $parameters = null)
+    {
+        if (!is_array($roleName)) {
+            $roleName = [$roleName];
+        }
+
+        $acl = $this->introspect();
+
+        foreach ($roleName as $name) {
+            if ($acl->isAllowed($name, $resourceName, $access, $parameters)) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    /**
+     * Introspect the access objects (, initialize the ACL adapter) and get ACL adapter.
+     *
+     * @return AdapterInterface
+     */
+    protected function introspect()
+    {
+        if (!$this->acl) {
+            /** @var \Phalcon\Cache\BackendInterface $cache */
+            $cache = $this->getDI()->getDataCache();
+            $acl   = $cache->get($this->config->get('cacheKey'));
+
+            if (!$acl || ENV_LOCAL === APPLICATION_ENV) {
+                $save = !$acl && ENV_PRODUCTION === APPLICATION_ENV;
+
+                /** @var \Phalcon\Acl\AdapterInterface $acl */
+                $adapter = $this->config->get('adapter');
+                $acl = new $adapter();
+                $acl->setDefaultAction(Acl::DENY);
+
+                /** @var Service\Role $rolesService */
+                $rolesService = $this->getDI()->getShared(Service\Role::class);
+                $roleNames = [];
+
+                foreach ($rolesService->getList() as $role) {
+                    $acl->addRole(new Acl\Role($role->name, $role->description));
+                    $roleNames[$role->id] = $role->name;
+                }
+
+                $acl->addResource(new Acl\Resource(self::ADMIN_AREA), 'access');
+                $acl->allow($rolesService->getOrCreateAdminRole()->getName(), self::ADMIN_AREA, 'access');
+
+                $objects = $this->addResources($acl);
+
+                foreach (Access::find() as $access) {
+                    /** @var Access $access */
+                    if (!isset($objects[$access->getObject()])) {
+                        continue;
+                    }
+
+                    $object = $objects[$access->getObject()];
+                    $value  = $access->getValue();
+
+                    if (in_array($access->getAction(), $object->actions) && ($value == 'allow' || $value == 'deny')) {
+                        $acl->$value($roleNames[$access->getRoleId()], $access->getObject(), $access->getAction());
+                    }
+                }
+
+                if ($save && !empty($objects)) {
+                    $cache->save($this->config->get('cacheKey'), $acl, $this->config->get('lifetime'));
+                }
+            }
+
+            $this->acl = $acl;
+        }
+
+        return $this->acl;
     }
 
     /**
