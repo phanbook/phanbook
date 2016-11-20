03ccbb8d4e620032bb592aea8845a5419da85f94
Serghei Iakovlev <serghei@phalconphp.com>
2016-11-20 11:44:27 +0200

    Amended error routing

--- core/modules/error/config/routing.php
+++ core/modules/error/config/routing.php
@@ -18,7 +18,22 @@ $error = new RouterGroup([
     'namespace' => 'Phanbook\Error\Controllers',
 ]);
 
+$error->addGet('/bad-request', 'Index::show400')
+      ->setName('bad-request');
+
+$error->addGet('/unauthorized', 'Index::show401')
+      ->setName('unauthorized');
+
+$error->addGet('/forbidden', 'Index::show403')
+      ->setName('forbidden');
+
 $error->addGet('/page-not-found', 'Index::show404')
     ->setName('page-not-found');
+
+$error->addGet('/internal-error', 'Index::show500')
+    ->setName('internal-error');
+
+$error->addGet('/maintenance', 'Index::show503')
+    ->setName('maintenance');
 
 return $error;
